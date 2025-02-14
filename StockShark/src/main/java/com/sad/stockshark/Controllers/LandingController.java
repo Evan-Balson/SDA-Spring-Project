46a9fcf55ec29189c2610bd19cf5ab8defba1710
Productivity-Square <153284064+Evan-Balson@users.noreply.github.com>
2025-02-14 14:57:38 -0400

    Sync Changes

--- StockShark/src/main/java/com/sad/stockshark/Controllers/LandingController.java
+++ StockShark/src/main/java/com/sad/stockshark/Controllers/LandingController.java
@@ -3,7 +3,7 @@ package com.sad.stockshark;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LandingController {
     @FXML
     private Label welcomeText;
 
