702280329d37c910ecb10cdeb34c39623597d434
maahia rahman <maahia.r@outlook.com>
2025-02-14 16:45:25 +0000

    Create Controller and fxml file for stock graphs

---
+++ StockShark/src/main/java/com/sad/stockshark/classes/StockGraphController.java
@@ -0,0 +1,7 @@
+package com.sad.stockshark.classes;
+
+public class StockGraphController {
+    public void generateGraph(String stockSymbol) {
+        System.out.println("Generating graph for: " + stockSymbol);
+    }
+}
