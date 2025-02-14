65a5562eb4f4d99022c6c23d51a0e1327ebecdd3
maahia rahman <maahia.r@outlook.com>
2025-02-14 14:58:43 +0000

    Adding remaining interface files

---
+++ StockShark/src/main/java/com/sad/stockshark/classes/iNotification.java
@@ -0,0 +1,5 @@
+package com.sad.stockshark.classes;
+
+public interface iNotification {
+    void sendNotification(String message);
+}
---
+++ StockShark/src/main/java/com/sad/stockshark/classes/iStockComparison.java
@@ -0,0 +1,5 @@
+package com.sad.stockshark.classes;
+
+public interface iStockComparison {
+    void compareStocks(String stock1, String stock2);
+}
