0bd521fe3eb0d54ccc3fcc3e00cf7ce6bb6349df
Hala Bakhtiar <halabakhtiar@gmail.com>
2025-02-19 12:29:16 +0000

    Removing the old use case diagram

--- StockShark/src/main/java/com/sad/stockshark/classes/StockComparisonSession.java
+++ StockShark/src/main/java/com/sad/stockshark/classes/StockComparisonSession.java
@@ -1,38 +1,9 @@
 package com.sad.stockshark.classes;
 
-public class StockComparisonSession extends Data implements iStockComparison{
+public class StockComparisonSession implements iStockComparison{
 
-    // Properties
-    private float currentPrice;
-    private String stock1;
-    private String stock2;
+    @Override
+    public void compareStocks(String stock1, String stock2) {
 
-    // Constructor
-    public StockComparisonSession(String name, float currentPrice, String symbol, String stock1, String stock2) {
-        super(name, symbol); // Calls the superclass (Data) constructor
-        this.currentPrice = currentPrice;
-        this.stock1 = stock1;
-        this.stock2 = stock2;
-    }
-
-    // Getter for symbol
-    public String symbol(){
-        return symbol;
-    }
-
-    // Getter for stock1
-     public String getStock1(){
-        return stock1;
-    }
-
-    // Getters for stock2
-    public String getStock2(){
-        return stock2;
-    }
-
-
-    // Compare stocks
-    public void compareStocks(String stock1, String stock2){
-        return "Comparing result between " + stock1 + " and " + stock2;
     }
 }
