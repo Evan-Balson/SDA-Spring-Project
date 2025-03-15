24b19102bfa78a4e0f4afe9f1b9a92bd77551d71
m12rezaei <m12rezaei@yahoo.com>
2025-03-15 22:38:02 +0000

    Update: stockComparison.java, Notification.java & Add iDatabase.java

--- StockShark/src/main/java/com/sad/stockshark/classes/StockComparisonSession.java
+++ StockShark/src/main/java/com/sad/stockshark/classes/StockComparisonSession.java
@@ -3,35 +3,20 @@ package com.sad.stockshark.classes;
 
 public class StockComparisonSession implements iStockComparison{
 
-    private iData idataInstance;
-    private iUserSession iusersessionInstance;
+    private iNotification notificationInstance;
+    private iUserSession usersessionInstance;
 
     // Properties
-    private float currentPrice;
     private String stock1;
     private String stock2;
-    private String name;
-    private String symbol;
+
 
     // Constructor required interfaces iData and iNotification
-    public StockComparisonSession( iData iD, iUserSession iU, String name, float currentPrice, String symbol, String stock1, String stock2) {
-        this.idataInstance = iD;
-        this.iusersessionInstance = iU;
-        this.name = name;
-        this.symbol = symbol;
-        this.currentPrice = currentPrice;
+    public StockComparisonSession( iNotification iN, iUserSession iU, String stock1, String stock2) {
+        this.iNotification = iN;
+        this.usersessionInstance = iU;
         this.stock1 = stock1;
         this.stock2 = stock2;
-    }
-    // Getter for name
-    public String getName() {
-        return name;
-    }
-
-
-    // Getter for symbol
-    public String getSymbol(){
-        return symbol;
     }
 
     // Getter for stock1
@@ -46,9 +31,9 @@ public class StockComparisonSession implements iStockComparison{
 
 
     // Compare stocks
-    public String compareStocks(){
-        return "Comparing result between " + this.stock1 + " and " + this.stock2;
+    @Override
+    public String compareStocks(String stock1, String stock2){
+        String result = "Comparing result between " + this.stock1 + " and " + this.stock2;
+        return result;
     }
-public class StockComparisonSession {
-
 }
--- StockShark/src/main/java/com/sad/stockshark/classes/iNotification.java
+++ StockShark/src/main/java/com/sad/stockshark/classes/iNotification.java
@@ -2,5 +2,4 @@ package com.sad.stockshark.classes;
 
 public interface iNotification {
     void sendNotification(String message);
-
 }
